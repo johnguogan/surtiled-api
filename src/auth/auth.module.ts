429894aac9d3f9fd76af939752c64a26254f26b5
johnguogan <johnguogan@outlook.com>
2022-04-15 06:30:37 +0300

    authentication add

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStretegy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1200s'},
+    })
+  ],
+  providers: [AuthService, LocalStrategy, LocalStrategy, JwtStretegy],
+  exports: [AuthService],
+})
+export class AuthModule {}
